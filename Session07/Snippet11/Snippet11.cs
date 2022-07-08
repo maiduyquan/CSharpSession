2db95692d4b30e7b69ca017ad638f8cac3223693
maiduyquan <91303493+maiduyquan@users.noreply.github.com>
2022-07-08 10:31:42 +0700

    Add files via upload

---
+++ Session07/Snippet11/Snippet11.cs
@@ -0,0 +1,33 @@
+﻿using System;
+interface ISet
+{
+    void AcceptDetails(int valOne, string valTwo);
+}
+interface IGet
+{
+    void Display();
+}
+class Employee: ISet
+{
+    int _empID;
+    string _empName;
+    public void AcceptDetails(int valOne,string valTwo)
+    {
+        _empID = valOne; 
+        _empName = valTwo;
+    }
+    static void Main(string[] args)
+    {
+        Employee objEm = new Employee();
+        objEm.AcceptDetails(10, "jack");
+        IGet objGet = objEm as IGet;
+        if(objGet != null)
+        {
+            objGet.Display();
+        }
+        else
+        {
+            Console.WriteLine("Invalid casring occurred");
+        }
+    }
+}
