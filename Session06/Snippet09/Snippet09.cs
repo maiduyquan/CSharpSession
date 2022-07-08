959a4d58dc8b8b7648764f6a2ce0a0cb18bcfcfd
maiduyquan <91303493+maiduyquan@users.noreply.github.com>
2022-07-08 10:22:30 +0700

    Add files via upload

---
+++ Session06/Snippet09/Snippet09.cs
@@ -0,0 +1,27 @@
+﻿class Student
+{
+    string _studentName = "james";
+    string _address = "Califonia";
+    public virtual void PrintDetails()
+    {
+        Console.WriteLine("Studnet Name: " + _studentName);
+        Console.WriteLine("Address: " + _address);
+    }
+}
+class Grade : Student
+{
+    string _class = "Four";
+    float _percent = 71.25F;
+    public override void PrintDetails()
+    {
+        Console.WriteLine("Class: " + _class);
+        Console.WriteLine("Percentage: " + _percent);
+    }
+    static void Main(string[] args)
+    {
+        Student objStudent = new Student();
+        Grade objGrade = new Grade();
+        objStudent.PrintDetails();
+        objGrade.PrintDetails();
+    }
+}
